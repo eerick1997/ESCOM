4aae8e09604912d7388068b5d9b87e579cce8b3d
Erick Efraín <vargas.erick030997@gmail.com>
2019-01-26 01:03:53 -0600

    Update

--- Cryptography/Tareas/Tarea01/cifrador.c
+++ Cryptography/Tareas/Tarea01/cifrador.c
@@ -3,17 +3,69 @@
 #include <stdlib.h>
 #include "headers/cipher.h"
 
+void choose(int option);
+
 int main(){
+    int option;
+    while(1){
+        printf("\n - Please select an option - \n");
+        printf("1. Ciphe plain text\n");
+        printf("2. Decode ciphe text\n");
+        printf("3. Brute force in ciphe text\n");
+        printf("4. Exit\n");
+        printf(" -> ");
+        scanf("%d", &option);
+        choose(option);
+    }
+    return 0;
+}
+
+void choose(int option){
+    int k, i;
     char input[MAX_LENGTH];
     char *text_cipher, *plain_text;
-    int k;
-    printf("Write the string to ciphe: ");
-    scanf("%[^\n]s", input);
-    printf("Shift value: ");
-    scanf("%d", &k);
-    text_cipher = cipher(input, k);
-    plain_text = decoder(text_cipher, k);
-    printf("cipher text: %s\n", text_cipher);
-    printf("plain text: %s\n", plain_text);
-    return 0;
+    printf("\n");
+    fflush(stdin);
+    switch (option)
+    {
+        case 1:
+            printf("Write the string to ciphe -> ");
+            scanf("%[^\n]s", input);
+            printf("Shift value -> ");
+            scanf("%d", &k);
+            text_cipher = cipher(input, k);
+            printf("cipher text: %s \n", text_cipher);
+            free(text_cipher);
+        break;
+
+        case 2:
+            printf("Write the string to decode -> ");
+            scanf("%[^\n]s", input);
+            printf("Shift value -> ");
+            scanf("%d", &k);
+            plain_text = decoder(input, k);
+            printf("plain text -> %s \n", plain_text);
+            free(plain_text);
+        break;
+
+        case 3:
+            printf("Write the string to decode -> ");
+            scanf("%[^\n]s", input);
+            for(i = 1; i < 26; i++){
+                plain_text = decoder(input, i);
+                printf("\n%d. plain text -> %s", i, plain_text);
+                free(plain_text);
+            }
+            printf("\n");
+        break;
+
+        case 4:
+            exit(0);
+        break;
+
+        default:
+            printf("Option %d doesn't exist", option);
+        break;
+        printf("\n");
+    }
 }
--- Cryptography/Tareas/Tarea01/codes/cipher.c
+++ Cryptography/Tareas/Tarea01/codes/cipher.c
@@ -17,19 +17,22 @@ char *cipher(char *m, int k){
     strcpy(c, m);
     for(i = 0; i < size; i++){
         if(c[i] != ' '){
-            printf("\n%d -> %c ", c[i], c[i]);
+            //printf("\n%d -> %c ", c[i], c[i]);
             if('a' <= c[i] && c[i] <= 'z'){
-                c[i] = ((c[i] + k) % 'z');
+                c[i] = ((c[i] + k) % ('z' + 1));
                 if( !('a' <= c[i] && c[i] <= 'z') ) 
-                    c[i] += 'a' - 1;
+                    c[i] = 'a' + c[i];
             }
             else if('A' <= c[i] && c[i] <= 'Z'){
-                c[i] = (c[i] + k) % 'Z';
+                c[i] = ((c[i] + k) % ('Z' + 1));
+                if( !('A' <= c[i] && c[i] <= 'Z') ) 
+                    c[i] = 'A' + c[i];
             }
-            printf("%d -> %c", c[i], c[i]);
+            //printf("%d -> %c", c[i], c[i]);
+            
         }
     }
-    printf("\n--------------");
+    //printf("\n--------------");
     text_cipher = malloc(sizeof(char)*size);
     strcpy(text_cipher, c);
     return text_cipher;   
@@ -49,18 +52,22 @@ char *decoder(char *c, int k){
     strcpy(m, c);
     for(i = 0; i < size; i++){
         if(m[i] != ' '){
-            printf("\n%d -> %c ", m[i], m[i]);
+            //printf("\n%d -> %c ", m[i], m[i]);
             if('a' <= c[i] && c[i] <= 'z'){
-                m[i] = abs((m[i] - k) % 'z');
+                m[i] = abs((m[i] - k) % ('z' + 1));
                 if( !('a' <= m[i] && m[i] <= 'z') ) 
-                    m[i] = 'z' - ('a' - m[i]) + 1;
-            } else if('A' <= c[i] && c[i] <= 'Z')
-                m[i] = abs((m[i] - k) % 'Z');
-            printf("%d -> %c", m[i], m[i]);
+                    m[i] = ('z' + 1) - abs('a' - m[i]);
+            } else if('A' <= c[i] && c[i] <= 'Z'){
+                m[i] = abs((m[i] - k) % ('Z' + 1));
+                if( !('A' <= m[i] && m[i] <= 'Z') ) 
+                    m[i] = ('Z' + 1) - abs('A' - m[i]);
+            }
+            //printf("%d -> %c", m[i], m[i]);
         }
     }
-    printf("\n--------------\n");
+    //printf("\n--------------\n");
     plain_text = malloc(sizeof(char)*size);
     strcpy(plain_text, m);
+    //printf("\n%s\n", m);
     return plain_text;   
 }
