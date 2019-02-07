53455283505b89dde3b0114b33f4d80278ca31cd
Erick Efraín <vargas.erick030997@gmail.com>
2019-02-06 19:52:43 -0600

    Update

--- Cryptography/Tareas/Tarea04/euclides_extendido.cpp
+++ Cryptography/Tareas/Tarea04/euclides_extendido.cpp
@@ -7,7 +7,9 @@ typedef long long int lli;
  * Para este caso se hace uso del teorema de Bezout ax + by = d es decir
  * ax + by = GCD(a, b) del GCD sabemos que recursivamente podemos calcularlo
  * con GCD(b, a % b), vease el programa que esta en la misma carpeta. Además
- * a % b = a - [a / b] b
+ * a % b = a - [a / b] b.
+ * Esta implementación también ayuda con la resolución de ecuaciones
+ * diofanticas: 
 */
 lli euclides_extendido(lli a, lli b, lli &x, lli &y, lli mod = 0){
 
@@ -27,11 +29,15 @@ lli euclides_extendido(lli a, lli b, lli &x, lli &y, lli mod = 0){
 
 int main(){
     lli x = 0, y = 0, a, b, GCD;
-    cin >> a >> b;
+    cout << "a -> ";
+    cin >> a;
+    cout << "\nb -> ";
+    cin >> b;
+    cout << endl;
     GCD = euclides_extendido(a, b, x, y);
     cout << "GCD: (" << a << "," << b << ") = " << GCD << endl;
     cout << "x: " << x << " y: " << y << endl;
     cout << GCD << " = " << a << "(" << x << ") " << "mod (" << b << ")" << endl;
-    cout << "res -> " << abs(y + x) << endl;
+    cout << "inv(a) -> " << x << endl;
     return 0;
 }
