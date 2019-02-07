4d0a343757ba7f67c5195829a751cc47409af21a
Erick Efraín <vargas.erick030997@gmail.com>
2019-02-06 21:45:50 -0600

    Update

--- Cryptography/Prácticas/Práctica 1/substitution.cpp
+++ Cryptography/Prácticas/Práctica 1/substitution.cpp
@@ -2,33 +2,66 @@
 
 using namespace std;
 
-string cipher(string plain_text, vector<int> &K){
-    string cipher_text = "";
-    int size = plain_text.size();
-    int size_keys = K.size();
-    for(int i = 0, j = 0; i  < size; i++, j++){
-        if(plain_text[i] != ' ')
-            cipher_text += ( ( ( plain_text[i] - 'a' ) + K[j % size_keys] ) % 26 ) + 'a';
-        else 
-            cipher_text += plain_text[i];
+string cipher(string c, vector<int> &K);
+string decoder(string m, vector<int> &K);
+void choose(string str, vector<int> &K);
+string read_file();
+
+int main(){
+    string str;
+    int k, v;
+    vector<int> K;
+    str = read_file();
+    cout << "How many keys we gonna use? -> ";
+    cin >> k;
+    while(k--){
+        cout << "Write the key " << (k + 1) << " -> ";
+        cin >> v;
+        K.push_back(v);
     }
-    return cipher_text;
+    choose(str, K);
+    return 0;
 }
 
-string decoder(string cipher_text, vector<int> &K){
+string cipher(string c, vector<int> &K){
+    string cipher_text = "";
+    int size = c.size();
+    int size_keys = K.size();
+    for(int i = 0, j = 0; i  < size; i++, j++){
+        if(c[i] != ' '){
+            if(islower(c[i])){
+                c[i] = ((c[i] + K[j % size_keys]) % ('z' + 1));
+                if( !islower(c[i]) ) 
+                    c[i] = 'a' + c[i];
+            }
+            else if(isupper(c[i])){
+                c[i] = ((c[i] + K[j % size_keys]) % ('Z' + 1));
+                if( !isupper(c[i])) 
+                    c[i] = 'A' + c[i];
+            }
+        }
+    }
+    return c;
+}   
+
+string decoder(string m, vector<int> &K){
     string plain_text = "";
-    int size = cipher_text.size();
+    int size = m.size();
     int size_keys = K.size();
     for(int i = 0, j = 0; i < size; i++, j++){
-        if(cipher_text[i] != ' '){
-            plain_text += ( ( ( cipher_text[i] - 'a') - K[j % size_keys]) % 26 ) + 'a';
-            if(!islower(plain_text[i]))
-                plain_text[i] = ('z' + 1) - abs('a' - plain_text[i]);
+        if(m[i] != ' '){
+           if(islower(m[i])){
+                m[i] = abs((m[i] - K[j % size_keys]) % ('z' + 1));
+                if( !islower(m[i]) ) 
+                    m[i] = ('z' + 1) - abs('a' - m[i]);
+            } else if(isupper(m[i])){
+                m[i] = abs((m[i] - K[j % size_keys]) % ('Z' + 1));
+                if( !isupper(m[i]) ) 
+                    m[i] = ('Z' + 1) - abs('A' - m[i]);
+            }
         }
-        else 
-            plain_text += cipher_text[i];
     }
-    return plain_text;
+    return m;
 }   
 
 string read_file(){
@@ -59,31 +92,16 @@ void choose(string str, vector<int> &K){
     {
         //Cipher
         case 1:
+            //cout << "str -> " << str << endl;
             cout << cipher(str, K) << endl;
             break;
         //Decode
         case 2:
+            //cout << "str -> " << str << endl;
             cout << decoder(str, K) << endl;
             break;
         default:
             cout << "Option " << option << " doesn't exists" << endl;
             break;
     }
-}
-
-
-int main(){
-    string str;
-    int k, v;
-    vector<int> K;
-    str = read_file();
-    cout << "How many keys we gonna use? -> ";
-    cin >> k;
-    while(k--){
-        cout << "Write the key " << (k + 1) << " -> ";
-        cin >> v;
-        K.push_back(v);
-    }
-    choose(str, K);
-    return 0;
 }
