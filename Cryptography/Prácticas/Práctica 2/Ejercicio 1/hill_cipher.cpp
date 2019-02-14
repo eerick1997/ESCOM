5a7a9e1b386d28dcc649b68e27a6f43959ed0fdf
Erick Efraín <vargas.erick030997@gmail.com>
2019-02-13 18:52:06 -0600

    Update hill_cipher.cpp

--- Cryptography/Prácticas/Práctica 2/Ejercicio 1/hill_cipher.cpp
+++ Cryptography/Prácticas/Práctica 2/Ejercicio 1/hill_cipher.cpp
@@ -6,7 +6,7 @@ const int MAX_VAL = 127;
 const int MIN_VAL = 32;
 
 typedef long long int lli;
-vector<vector<lli>> matrix;
+typedef vector<vector<lli>> Matrix;
 
 lli euclides(lli a, lli b, lli &x, lli &y, lli mod = 0){
     if(!b){
@@ -28,13 +28,21 @@ lli inverse(int a, int b){
     return ans;
 }
 
-lli det(vector<vector<lli>> &matrix){
+Matrix getInverseMatrix(Matrix matrix, lli mod_inverse){
+
+    for(int i = 0; i < matrix.size(); i++)
+        for(int j = 0; j < matrix[i].size(); j++)
+            matrix[i][j] *= mod_inverse;
+    return matrix; 
+}
+
+lli det(Matrix &matrix){
     return matrix[0][0]*matrix[1][1] * matrix[2][2] + matrix[0][1]*matrix[1][2]*matrix[2][0] + matrix[0][2]*matrix[1][0]*matrix[2][1] 
     - matrix[0][2]*matrix[1][1]*matrix[2][0] - matrix[0][1]*matrix[1][0]*matrix[2][2] - matrix[0][0]*matrix[1][2]*matrix[2][1];
 }
 
-vector<vector<lli>> getMatrix(){
-    vector<vector<lli>> matrix(3, vector<lli>(3));
+Matrix getMatrix(){
+    Matrix matrix(3, vector<lli>(3));
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++)
             matrix[i][j] = rand() % 27;
@@ -42,17 +50,28 @@ vector<vector<lli>> getMatrix(){
     return matrix;
 }
 
-void imprimir(vector<vector<lli>> matrix){
+lli multiply(){
+    
+}
+
+string cipher(Matrix matrix, string str){
+    
+    for(int i = 0, j = 0; i < str.size(); i++){
+        
+    }
+}
+
+void imprimir(Matrix &matrix){
     for(int i = 0; i < matrix.size(); i++){
         cout << endl;
         for(int j = 0; j < matrix[i].size(); j++){
             cout << matrix[i][j] << " ";
-        }
+        }   
     }
 }
 
 int main(){
-    vector<vector<lli>> matrix = getMatrix();
+    Matrix matrix = getMatrix();
     cout << det(matrix) << endl;
     cout << "Modular inverse " << inverse(det(matrix), 96) << endl;
     return 0;
