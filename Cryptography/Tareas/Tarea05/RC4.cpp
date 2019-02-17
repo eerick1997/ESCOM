3fe141ef935dd9333b91846545e68ca2d32a4c38
Erick Efraín <vargas.erick030997@gmail.com>
2019-02-16 23:01:45 -0600

    Update

---
+++ Cryptography/Tareas/Tarea05/RC4.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define SIZE 32
+
+using namespace std;
+
+void f(){
+    vector<short> S(255, 0);
+    vector<short> T(255, 0);
+    vector<short> K(255, 0);
+    for(int i = 0; i < 255; i++){
+        S[i] = i;
+        T[i] = K[i % k]
+    }
+}
+
+int main(){
+
+    return 0;
+}
