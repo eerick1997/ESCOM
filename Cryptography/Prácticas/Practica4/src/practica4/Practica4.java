8cb8c2646293bbfc1d86ee5af7b4d74705ea025a
aesquivelv <albertoesquivel.97@gmail.com>
2019-03-10 14:46:49 -0600

    3DES

--- Cryptography/Prácticas/Practica4/src/practica4/Practica4.java
+++ Cryptography/Prácticas/Practica4/src/practica4/Practica4.java
@@ -1,9 +1,63 @@
 package practica4;
-
+import com.chilkatsoft.*;
 public class Practica4 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        CkCrypt2 crypt = new CkCrypt2();
+
+    boolean success = crypt.UnlockComponent("Anything for 30-day trial");
+    if (success != true) {
+        System.out.println(crypt.lastErrorText());
+        return;
+    }
+
+    //  Specify 3DES for the encryption algorithm:
+    crypt.put_CryptAlgorithm("3des");
+
+    //  CipherMode may be "ecb" or "cbc"
+    crypt.put_CipherMode("cbc");
+
+    //  KeyLength must be 192.  3DES is technically 168-bits;
+    //  the most-significant bit of each key byte is a parity bit,
+    //  so we must indicate a KeyLength of 192, which includes
+    //  the parity bits.
+    crypt.put_KeyLength(192);
+
+    //  The padding scheme determines the contents of the bytes
+    //  that are added to pad the result to a multiple of the
+    //  encryption algorithm's block size.  3DES has a block
+    //  size of 8 bytes, so encrypted output is always
+    //  a multiple of 8.
+    crypt.put_PaddingScheme(0);
+
+    //  EncodingMode specifies the encoding of the output for
+    //  encryption, and the input for decryption.
+    //  It may be "hex", "url", "base64", or "quoted-printable".
+    crypt.put_EncodingMode("hex");
+
+    //  An initialization vector is required if using CBC or CFB modes.
+    //  ECB mode does not use an IV.
+    //  The length of the IV is equal to the algorithm's block size.
+    //  It is NOT equal to the length of the key.
+    String ivHex = "0001020304050607";
+    crypt.SetEncodedIV(ivHex,"hex");
+
+    //  The secret key must equal the size of the key.  For
+    //  3DES, the key must be 24 bytes (i.e. 192-bits).
+    String keyHex = "000102030405060708090A0B0C0D0E0F0001020304050607";
+    crypt.SetEncodedKey(keyHex,"hex");
+
+    //  Encrypt a string...
+    //  The input string is 44 ANSI characters (i.e. 44 bytes), so
+    //  the output should be 48 bytes (a multiple of 8).
+    //  Because the output is a hex string, it should
+    //  be 96 characters long (2 chars per byte).
+    String encStr = crypt.encryptStringENC("The quick brown fox jumps over the lazy dog.");
+    System.out.println(encStr);
+
+    //  Now decrypt:
+    String decStr = crypt.decryptStringENC(encStr);
+    System.out.println(decStr);
     }
     
 }
--- Cryptography/Prácticas/Practica4/src/practica4/TripleDES.java
+++ Cryptography/Prácticas/Practica4/src/practica4/TripleDES.java
@@ -1,24 +1,64 @@
-package practica4;
-
 import java.security.MessageDigest;
 import java.util.Arrays;
+
+import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class TripleDES {
-    
-    public byte[] ciphe(byte[] object) throws Exception{
+
+    public static void main(String[] args) throws Exception {
+
+        String text = "kyle boon";
+
+        byte[] codedtext = new TripleDES().encrypt(text);
+        String decodedtext = new TripleDES().decrypt(codedtext);
+
+        System.out.println(codedtext); // this is a byte array, you'll just see a reference to an array
+        System.out.println(decodedtext); // This correctly shows "kyle boon"
+    }
+
+    public byte[] encrypt(String message) throws Exception {
         final MessageDigest md = MessageDigest.getInstance("md5");
         final byte[] digestOfPassword = md.digest("HG58YZ3CR9"
                 .getBytes("utf-8"));
         final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
-        for(int i = 0, j = 16; i < 8;){
-            keyBytes[j++] = keyBytes[i++];
+        for (int j = 0, k = 16; j < 8;) {
+            keyBytes[k++] = keyBytes[j++];
         }
-        
+
         final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
-        final IvParameterSpec IV = new IvParameterSpec(new byte[8]);
-        return null;
+        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+        final Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+        final byte[] plainTextBytes = message.getBytes("utf-8");
+        final byte[] cipherText = cipher.doFinal(plainTextBytes);
+        // final String encodedCipherText = new sun.misc.BASE64Encoder()
+        // .encode(cipherText);
+
+        return cipherText;
+    }
+
+    public String decrypt(byte[] message) throws Exception {
+        final MessageDigest md = MessageDigest.getInstance("md5");
+        final byte[] digestOfPassword = md.digest("HG58YZ3CR9"
+                .getBytes("utf-8"));
+        final byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
+        for (int j = 0, k = 16; j < 8;) {
+            keyBytes[k++] = keyBytes[j++];
+        }
+
+        final SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+        final IvParameterSpec iv = new IvParameterSpec(new byte[8]);
+        final Cipher decipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
+        decipher.init(Cipher.DECRYPT_MODE, key, iv);
+
+        // final byte[] encData = new
+        // sun.misc.BASE64Decoder().decodeBuffer(message);
+        final byte[] plainText = decipher.doFinal(message);
+
+        return new String(plainText, "UTF-8");
     }
 }
