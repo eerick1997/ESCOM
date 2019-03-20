66483167361b76abb2b9ad344fa92c133be6d7f7
Erick Efraín <vargas.erick030997@gmail.com>
2019-03-20 17:05:01 -0600

    Update

---
+++ Cryptography/Prácticas/Práctica 6/multiplication.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int mulltiply(int a, int b, int m){
+    int MSB = 1, i = 1, j = 0;
+    int ma = m;
+    int aa = a;
+    int ans = 0;
+    //Searching the MSB
+    while(ma >>= 1 != 0){
+        MSB++;
+    }
+
+    while( (aa >>= 1) != 0){
+        if( ( aa & 1 ) == 1 )
+            ans ^= (b << i);
+        i++;
+    }
+
+    if(ans > (pow(2, MSB + 1))){
+        ans ^= m;
+    }
+
+    cout << MSB << endl;
+    return ans;
+}
+
+int main(){
+
+    int a, b, m;
+    cin >> a >> b >> m;
+    cout << mulltiply(a, b, m);
+}
