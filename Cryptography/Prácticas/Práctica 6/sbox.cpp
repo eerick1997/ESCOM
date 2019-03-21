710022c521544a72ebaa83315adda5c0f9d8d7a9
Erick Efraín <vargas.erick030997@gmail.com>
2019-03-21 15:54:19 -0600

    Update

--- Cryptography/Prácticas/Práctica 6/sbox.cpp
+++ Cryptography/Prácticas/Práctica 6/sbox.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 #include "sbox.h"
 
+//143
+//199
+//227
+//241
+//248
+//124
+//62
+//31
+void generateSBox(){
+
+    for(int i = 0; i < 256; i++){
+        
+    }
+
+}
+
 int main(){
 
     return 0;
